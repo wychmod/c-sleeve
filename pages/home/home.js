2bbef6a5bbe44321c331d4116680e289490eddb5
wychmod <545480453@qq.com>
2021-02-23 21:10:53 +0800

    Add files via upload

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,99 +1,103 @@
 // pages/home/home.js
 
-import {Theme} from "../../model/theme";
-import {Banner} from "../../model/banner";
-import {Category} from "../../model/category";
-import {Activity} from "../../model/Activity";
+import {Theme} from "../../models/theme";
+import {Banner} from "../../models/banner";
+import {Category} from "../../models/category";
+import {Activity} from "../../models/activity";
+import {SpuPaging} from "../../models/spu-paging";
 
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    themeA: null,
-    bannerB: null,
-    grid: []
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        themeA: null,
+        themeE: null,
+        bannerB: null,
+        grid: [],
+        activityD: null,
+        spuPaging:null,
+        loadingType:'loading'
+    },
 
-  async onLoad(options) {
-    this.initAllData();
-  },
+    async onLoad(options) {
+        this.initAllData()
+        this.initBottomSpuList()
+    },
 
-  async initAllData() {
-    const theme = new Theme();
-    await theme.getThemes();
-    const themeA = theme.getHomeLocationA();
-    const themeE = theme.getHomeLocationE();
-    let themeESpu = [];
+    async initBottomSpuList() {
+        const paging = SpuPaging.getLatestPaging()
+        this.data.spuPaging = paging
+        const data = await paging.getMoreData()
+        if (!data) {
+            return
+        }
 
-    if (themeE.online) {
-      const data = await Theme.getHomeLocationESpu();
-      if (data) {
-        themeESpu = data.spu_list.slice(0, 8)
-      }
+        wx.lin.renderWaterFlow(data.items)
+
+
+    },
+
+    async initAllData() {
+        const theme = new Theme()
+        await theme.getThemes()
+
+        const themeA = theme.getHomeLocationA()
+        const themeE = theme.getHomeLocationE()
+        let themeESpu = []
+
+        if (themeE.online) {
+            const data = await Theme.getHomeLocationESpu()
+            if (data) {
+                themeESpu = data.spu_list.slice(0, 8)
+            }
+        }
+
+        const themeF = theme.getHomeLocationF()
+
+        const bannerB = await Banner.getHomeLocationB()
+        const grid = await Category.getHomeLocationC()
+        const activityD = await Activity.getHomeLocationD()
+
+        const bannerG = await Banner.getHomeLocationG()
+
+        const themeH = theme.getHomeLocationH()
+
+        this.setData({
+            themeA,
+            bannerB,
+            grid,
+            activityD,
+            themeE,
+            themeESpu,
+            themeF,
+            bannerG,
+            themeH
+        })
+    },
+
+    onReachBottom: async function () {
+        const data = await this.data.spuPaging.getMoreData()
+        if(!data){
+            return
+        }
+        wx.lin.renderWaterFlow(data.items)
+        if(!data.moreData){
+            this.setData({
+                loadingType:'end'
+            })
+        }
+    },
+
+    onPullDownRefresh: function () {
+
+    },
+
+
+    onShareAppMessage: function () {
+
     }
-    const themeF = theme.getHomeLocationF()
-    const bannerB = await Banner.getHomeLocationB();
-    const grid = await Category.getHomeLocationC();
-    const activityD = await Activity.getHomeLocationD();
-    this.setData({
-      themeA,
-      themeE,
-      themeESpu,
-      themeF,
-      bannerB,
-      grid,
-      activityD
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
+
+
